652e90f11cb1a6746e475d8e5da6a019db3f5b8c
kobblander <kobblander@gmail.com>
2014-10-31 18:48:53 +0000

    Adding json simple

---
+++ src/is/ru/app/CarCollector/cars/data/dto/SybType.java
@@ -0,0 +1,16 @@
+package is.ru.app.CarCollector.cars.data.dto;
+
+/**
+ * <h1>SybType</h1>
+ * <h2>is.ru.app.CarCollector.cars.data.dto</h2>
+ * <p></p>
+ * Created on 31.10.2014.
+ *
+ * @author jakob
+ * @version 1.1
+ */
+public class SybType {
+
+    SybType() {
+    }
+}
